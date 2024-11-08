9a93221303c962f3f7f26e52f9b896ad6dc08b96
Shyam Kumar <shyamprajapatiofficial2011@gmail.com>
2024-11-08 15:10:58 +0530

    fixed issues

--- getcases_full.php
+++ getcases_full.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 if (session_status() == PHP_SESSION_NONE) {
-    session_set_cookie_params(['samesite' => 'None']);s]
+    session_set_cookie_params(['samesite' => 'None']);
     session_start();
 }
 
--- login.php
+++ login.php
@@ -3,10 +3,18 @@
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('Access-Control-Allow-Origin: http://localhost:5173');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    header('Access-Control-Max-Age: 86400'); // Cache preflight response for 24 hours
+    exit(0); // End the request
+}
 // Configure session settings
-session_set_cookie_params(['samesite' => 'None']);
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_set_cookie_params(['samesite' => 'None']);
+    session_start();
+}
 
 include 'database.php';
 include 'utils.php';
